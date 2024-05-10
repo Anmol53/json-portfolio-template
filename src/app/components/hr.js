2484e0b94d54e2e877d27becac5e71dc84120999
Anmol Agrawal <anmol.ag53@gmail.com>
2024-05-11 02:59:05 +0530

    improved hr

--- src/app/components/hr.js
+++ src/app/components/hr.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const StyledHR = styled.div`
   padding: 0;
   width: 10%;
-  border: dotted ${({ theme }) => theme.accentSecondaryColor} 6px;
+  border: 2px solid ${({ theme }) => theme.accentSecondaryColor};
   border-bottom: none;
   margin: 2rem auto;
   grid-column: span 2;
