582459568cc309a50100ec8c3130302045f04f8f
Anmol Agrawal <anmol.ag53@gmail.com>
2024-05-11 19:09:12 +0530

    removed unnecessary code

--- src/app/components/section.js
+++ src/app/components/section.js
@@ -90,38 +90,6 @@ const SubsectionContainer = styled.div`
   }
 `;
 
-const Buttons = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-  padding: 1.5rem 0;
-  gap: 1rem;
-`;
-
-const StyledButton = styled.a`
-  text-decoration: none;
-  font-size: 1.5rem;
-  color: ${({ theme }) => theme.primaryTextColor};
-  background: linear-gradient(
-    163deg,
-    ${({ theme }) => theme.accentSecondaryColor} 50%,
-    ${({ theme }) => theme.accentColor} 100%
-  );
-  padding: 0.75rem 1.5rem;
-  border: none;
-  border-radius: 1rem;
-  transition: 1000ms;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  cursor: pointer;
-
-  &:hover {
-    transition: 1000ms;
-    padding: 1rem 2rem;
-  }
-`;
-
 export default function Section({ data, $index }) {
   return (
     <section id={data.name}>
